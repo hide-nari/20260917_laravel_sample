bb49465ce51bf94bbc2413fe8a8aa9eef1883876
hide-nari <hide3@mild.ocn.ne.jp>
2026-09-18 06:06:09 +0900

    Mod user factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name'  => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name'  => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
 
         for ($i = 1; $i <= 10; $i++) {
             Department::create([
