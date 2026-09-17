71336d8f8ac158a40fb31e8edf24591fa85aa544
hide-nari <hide3@mild.ocn.ne.jp>
2026-09-18 05:16:33 +0900

    Add department model

---
+++ database/factories/departmentFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+class departmentFactory extends Factory
+{
+    protected $model = department::class;
+
+    public function definition(): array
+    {
+        return [
+            'name'       => $this->faker->name(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\department;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,8 +19,26 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name'  => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        for ($i = 1; $i <= 10; $i++) {
+            Department::create([
+                'name' => '営業'.$i.'部',
+            ]);
+        }
+        for ($i = 1; $i <= 10; $i++) {
+            Department::create([
+                'name' => '管理'.$i.'部',
+            ]);
+        }
+        for ($i = 1; $i <= 10; $i++) {
+            Department::create([
+                'name' => 'システム'.$i.'部',
+            ]);
+        }
+
+        Department::factory(10)->create();
     }
 }
